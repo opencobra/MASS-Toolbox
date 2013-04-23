76d4afbee87f4cbf7c30185a00e7723917d320f3
Nikolaus Sonnenschein <niko.sonnenschein@googlemail.com>
2013-04-23 14:25:46 -0700

    Working prototype of qcqa.

--- Toolbox/QCQA.m
+++ Toolbox/QCQA.m
@@ -24,7 +24,7 @@ getSuperfluous[model_MASSmodel]:=Module[{ode},
 
 
 Options[qcqa]={"MissingParameters"->True,"MissingInitialConditions"->True,"SuperfluousParameters"->True,"Rates"->True,"ODE"->True,"MissingParameterStyle"->(Style[#,Background->Red,Bold]&),"MissingInitialConditionStyle"->(Style[#,Background->Orange,Bold]&)};
-qcqa[model_MASSmodel,opts:OptionsPattern[]]:=Module[{missingParameters,pane,grid,rates,missingIC,$MAXROWS,report,prune},
+qcqa[model_MASSmodel,opts:OptionsPattern[]]:=Module[{missingParameters,pane,grid,rates,missingIC,$MAXROWS,report,prune,superfluousParam,rateWithMissingParamAndIC,odeWithMissingParamAndIC},
 	$MAXROWS=50;
 	prune=If[Length[#]>$MAXROWS,Join[#[[1;;$MAXROWS]],{{SpanFromLeft,ToString[Length[#]-$MAXROWS]<>" rows omitted!",SpanFromBoth}}],#]&;
 	pane=Pane[#,{{800},{600}},Scrollbars->{True,True}]&;
@@ -34,27 +34,42 @@ qcqa[model_MASSmodel,opts:OptionsPattern[]]:=Module[{missingParameters,pane,grid
 
 	If[OptionValue["MissingParameters"]===True,
 		missingParameters=getMissingParameters[model,rates];
-		AppendTo[report,"Missing parameters"->pane@grid@Join[{{"Parameter","Rate equation(s)"}},Table[{p,Column[Select[rates,MemberQ[#,p]&]]/.p:>OptionValue["MissingParameterStyle"][p]},{p,missingParameters}]]]
+		If[missingParameters=!={},
+			AppendTo[report,"Missing parameters"->pane@grid@Join[{{"Parameter","Rate equation(s)"}},Table[{p,Column[Select[rates,MemberQ[#,p]&]]/.p:>OptionValue["MissingParameterStyle"][p]},{p,missingParameters}]]];
+		];
 	];
 
 	If[OptionValue["MissingInitialConditions"]===True,
 		missingIC=getMissingInitialConditions[model,rates];
-		AppendTo[report,"Missing initial conditions"->pane@grid@Join[{{"Variable","Rate equation(s)"}},Table[{ic,Column[prune@Select[rates,MemberQ[#,ic,\[Infinity]]&]]/.ic:>OptionValue["MissingInitialConditionStyle"][ic]},{ic,missingIC}]]]
+		If[missingIC=!={},
+			AppendTo[report,"Missing initial conditions"->pane@grid@Join[{{"Variable","Rate equation(s)"}},Table[{ic,Column[prune@Select[rates,MemberQ[#,ic,\[Infinity]]&]]/.ic:>OptionValue["MissingInitialConditionStyle"][ic]},{ic,missingIC}]]];
+		];
 	];
 
 	If[OptionValue["SuperfluousParameters"]===True,
-		AppendTo[report,"Superfluous parameters"->pane@grid@(List@@@getSuperfluous[model])]
+		superfluousParam=getSuperfluous[model];
+		If[superfluousParam=!={},
+			AppendTo[report,"Superfluous parameters"->pane@grid@(List@@@superfluousParam)];
+		];
 	];
 
 	If[OptionValue["Rates"]===True,
-		AppendTo[report,"Rates with missing parameters and initial conditions"->pane@grid@Partition[Select[rates/.Join[(#->OptionValue["MissingInitialConditionStyle"][#]&/@missingIC),(#->OptionValue["MissingParameterStyle"][#]&/@missingParameters)(*,{pat:Join[$MASS$parametersPattern,$MASS$speciesPattern]:>Style[pat,Opacity[.5]]}*)],MemberQ[#,_Style,\[Infinity]]&],1]];
+		rateWithMissingParamAndIC=Select[rates/.Join[(#->OptionValue["MissingInitialConditionStyle"][#]&/@missingIC),(#->OptionValue["MissingParameterStyle"][#]&/@missingParameters)],MemberQ[#,_Style,\[Infinity]]&];
+		If[rateWithMissingParamAndIC=!={},
+			AppendTo[report,"Rates with missing parameters and initial conditions"->pane@grid@Partition[rateWithMissingParamAndIC,1]];
+		];
 	];
 
 	If[OptionValue["ODE"]===True,
-		AppendTo[report,"ODEs with missing parameters and initial conditions"->pane@grid@Partition[Select[model["ODE"]/.Join[(#->OptionValue["MissingInitialConditionStyle"][#]&/@missingIC),(#->OptionValue["MissingParameterStyle"][#]&/@missingParameters)(*,{pat:Join[$MASS$parametersPattern,$MASS$speciesPattern]:>Style[pat,Opacity[.5]]}*)],MemberQ[#,_Style,\[Infinity]]&],1]];
+		odeWithMissingParamAndIC=Select[model["ODE"]/.Join[(#->OptionValue["MissingInitialConditionStyle"][#]&/@missingIC),(#->OptionValue["MissingParameterStyle"][#]&/@missingParameters)],MemberQ[#,_Style,\[Infinity]]&];
+		If[rateWithMissingParamAndIC=!={},
+			AppendTo[report,"ODEs with missing parameters and initial conditions"->pane@grid@Partition[odeWithMissingParamAndIC,1]];
+		];
 	];
-	Print[report];
-	MenuView[report,ImageSize->Automatic]
+	If[report==={},
+		Panel["No issues detected :-)"],
+		MenuView[report,ImageSize->Automatic]
+	]
 ];
 
 
