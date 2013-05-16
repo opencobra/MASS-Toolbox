08f1b9bb98941a549c1a6b45345ea0e212129d7b
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2013-05-15 21:44:56 -0700

    Fancy new logo.

--- Toolbox/Kernel/init.m
+++ Toolbox/Kernel/init.m
@@ -25,8 +25,13 @@ AutoCollapse[]:=(If[$FrontEnd=!=$Failed,SelectionMove[EvaluationNotebook[],All,G
 (*Load defintions*)
 
 
+(*Column[{icon,progtext,If[$FrontEnd=!=Null,ProgressIndicator[prog,{1,22}],prog]}]*)
+
+
+icon=Show[Import[FileNameJoin[{DirectoryName[$InputFileName],"MASS-Toolbox-Logo.m"}]],ImageSize->150];
+delay=Pause[.07]&;
 If[$FrontEnd=!=Null,
-	Monitor[ReleaseHold[#],Column[{progtext,If[$FrontEnd=!=Null,ProgressIndicator[prog,{1,22}],prog]}]],
+	Monitor[ReleaseHold[#],Row[{Blur[icon,(*Log[23-prog]*)Max[{15-prog,0}]],progtext}]],
 	ReleaseHold[#]
 ]&@Hold[
 	prog=0;
@@ -43,7 +48,7 @@ If[$FrontEnd=!=Null,
 	prog++;
 	
 	progtext="Loading GurobiML ...";
-	Quiet@Needs["GurobiML`"];prog++;
+	Quiet@Needs["GurobiML`"];prog++;delay[];
 
 	progtext="Loading AutomaticUnits ...";
 	Unprotect[BeginPackage];
@@ -53,30 +58,30 @@ If[$FrontEnd=!=Null,
 	PhysicalConstants`Private`Mole=AutomaticUnits`Unit[1,"Mole"];
 	BeginPackage["PhysicalConstants`", "Units`"] =.;
 	Protect[BeginPackage];
-	progtext="Loading AutomaticUnits ...";Needs["AutomaticUnits`"];prog++;
+	progtext="Loading AutomaticUnits ...";Needs["AutomaticUnits`"];prog++;delay[];
 	
-	progtext="Loading InterpolatingFunctionAnatomy ...";Needs["DifferentialEquations`InterpolatingFunctionAnatomy`"];prog++;
-	progtext="XML ...";Needs["XML`"];prog++;
-	progtext="Loading JLink ...";Needs["JLink`"];prog++;
+	progtext="Loading InterpolatingFunctionAnatomy ...";Needs["DifferentialEquations`InterpolatingFunctionAnatomy`"];prog++;delay[];
+	progtext="XML ...";Needs["XML`"];prog++;delay[];
+	progtext="Loading JLink ...";Needs["JLink`"];prog++;delay[];
 	
 	BeginPackage["Toolbox`"];
 	Needs["AutomaticUnits`"];
-	progtext="Loading Config ...";Get["Toolbox`Config`"];prog++;
-	progtext="Loading Usage strings ...";Get["Toolbox`UsageStrings`"];prog++;
-	progtext="Loading Utilities ...";Get["Toolbox`Util`"];prog++;
-	progtext="Loading Types ...";Get["Toolbox`Types`"];prog++;
-	progtext="Loading Core ...";Get["Toolbox`Core`"];prog++;
-	progtext="Loading IO ...";Get["Toolbox`IO`"];prog++;
-	progtext="Loading COBRA ...";Get["Toolbox`COBRA`"];prog++;
-	progtext="Loading Chemoinformatics ...";Get["Toolbox`Chemoinformatics`"];prog++;
-	progtext="Loading Visualization ...";Get["Toolbox`Visualization`"];prog++;
-	progtext="Loading Regulation ...";Get["Toolbox`Regulation`"];prog++;
-	progtext="Loading Sensitivity Analysis ...";Get["Toolbox`Sensitivity`"];prog++;
-	progtext="Loading Thermodynamics ...";Get["Toolbox`Thermodynamics`"];prog++;
-	progtext="Loading Network Theory ...";Get["Toolbox`Networks`"];prog++;
-	progtext="Loading Simulations ...";Get["Toolbox`Simulations`"];prog++;
-	progtext="Loading QCQA ...";Get["Toolbox`QCQA`"];prog++;
-	progtext="Loading ExampleData ...";Get["Toolbox`ExampleData`"];prog++;
+	progtext="Loading Config ...";Get["Toolbox`Config`"];prog++;delay[];
+	progtext="Loading Usage strings ...";Get["Toolbox`UsageStrings`"];prog++;delay[];
+	progtext="Loading Utilities ...";Get["Toolbox`Util`"];prog++;delay[];
+	progtext="Loading Types ...";Get["Toolbox`Types`"];prog++;delay[];
+	progtext="Loading Core ...";Get["Toolbox`Core`"];prog++;delay[];
+	progtext="Loading IO ...";Get["Toolbox`IO`"];prog++;delay[];
+	progtext="Loading COBRA ...";Get["Toolbox`COBRA`"];prog++;delay[];
+	progtext="Loading Chemoinformatics ...";Get["Toolbox`Chemoinformatics`"];prog++;delay[];
+	progtext="Loading Visualization ...";Get["Toolbox`Visualization`"];prog++;delay[];
+	progtext="Loading Regulation ...";Get["Toolbox`Regulation`"];prog++;delay[];
+	progtext="Loading Sensitivity Analysis ...";Get["Toolbox`Sensitivity`"];prog++;delay[];
+	progtext="Loading Thermodynamics ...";Get["Toolbox`Thermodynamics`"];prog++;delay[];
+	progtext="Loading Network Theory ...";Get["Toolbox`Networks`"];prog++;delay[];
+	progtext="Loading Simulations ...";Get["Toolbox`Simulations`"];prog++;delay[];
+	progtext="Loading QCQA ...";Get["Toolbox`QCQA`"];prog++;delay[];
+	progtext="Loading ExampleData ...";Get["Toolbox`ExampleData`"];prog++;delay[];
 	EndPackage[];
 ]
 
@@ -89,3 +94,13 @@ Toolbox`MASS`metabolite := metabolite
 Toolbox`MASS`v := v
 Toolbox`MASS`Keq := Keq
 Toolbox`MASS`rateconst := rateconst
+
+MASSToolbox`MASS`metabolite := metabolite
+MASSToolbox`MASS`v := v
+MASSToolbox`MASS`Keq := Keq
+MASSToolbox`MASS`rateconst := rateconst
+
+MASSToolbox`metabolite := metabolite
+MASSToolbox`v := v
+MASSToolbox`Keq := Keq
+MASSToolbox`rateconst := rateconst
