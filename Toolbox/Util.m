14af4e6e170f593c8a5cc6ba153a8b038e16472a
Anand <avsastry@ucsd.edu>
2014-12-09 11:13:25 -0800

    Fixed ProgressIndicator[ComplexInfinity] error

--- Toolbox/Util.m
+++ Toolbox/Util.m
@@ -185,6 +185,7 @@ updateToolbox[version_String,OptionsPattern[]]:=
 		];
 
 		(* Download new version *)
+		Clear[Global`progress];
 		directory = $UserDocumentsDirectory;
 		fileName=directory<>"/MASS-Toolbox-"<>version<>".tar.gz";
 		Print["Please wait. Downloading Toolbox v"<>version<>"..."];
